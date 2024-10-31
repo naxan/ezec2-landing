26f227528769840ffb2e323225a0ac1cd645b362
akempel <anna.kb.kempel@gmail.com>
2024-10-31 04:49:07 -0400

    Adds new logos

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,10 +17,10 @@
       // === logo change
       if (ud_header.classList.contains("sticky")) {
         document.querySelector(".header-logo").src =
-          "assets/images/logo/logo.svg"
+          "assets/images/logo/logo-b-light.png"
       } else {
         document.querySelector(".header-logo").src =
-          "assets/images/logo/logo-white.svg"
+          "assets/images/logo/logo-b-dark.png"
       }
     }
 
@@ -29,7 +29,7 @@
         // === logo change
         if (ud_header.classList.contains("sticky")) {
           document.querySelector(".header-logo").src =
-            "assets/images/logo/logo-white.svg"
+            "assets/images/logo/logo-b-dark.png"
         } 
       }
     }
